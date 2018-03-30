9136c5b076311480e030adb2f4ebd159d4d8978e
Byron Sanchez <byron@hackbytes.io>
2018-03-30 01:22:48 +0000

    Update ports in docker scripts so they don't collide with hackbytes.io

--- nitelite.io-scripts/docker-run.ps1
+++ nitelite.io-scripts/docker-run.ps1
@@ -1,2 +1,2 @@
 
-docker run --init -it --rm -p 8080:8080 -p 35729:35729 -v ${pwd}/nitelite.io-web:/home/wintersmith/nitelite.io -m 300M --memory-swap 1G byronsanchez/nitelite.io $args
+docker run --init -it --rm -p 8084:8080 -p 35729:35729 -v ${pwd}/nitelite.io-web:/home/wintersmith/nitelite.io -m 300M --memory-swap 1G byronsanchez/nitelite.io $args
